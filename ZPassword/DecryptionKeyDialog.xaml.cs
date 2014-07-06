01019afd93f2fe6fcd64005e0b4c7926df340707
Zed Coder <makandok@yahoo.com>
2014-07-06 22:39:48 +0200

    Contains the various preliminary forms needed
    Yet to tie it all together

---
+++ ZPassword/DecryptionKeyDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Z10PasswordVault
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class DecryptionKeyDialog : Window
+    {
+        public DecryptionKeyDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key==Key.Return)
+            {
+                DialogResult = true;
+            }
+        }
+
+        //public string 
+    }
+}
---
+++ ZPassword/ViewAllSites.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Z10PasswordVault
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ViewAllSites : Window
+    {
+        public ViewAllSites()
+        {
+            //InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var showMasterPasswordDialog = new DecryptionKeyDialog();
+            var dialogResult = showMasterPasswordDialog.ShowDialog();
+            if (dialogResult.HasValue && dialogResult.Value && !string.IsNullOrWhiteSpace(showMasterPasswordDialog.txtPassword.Password))
+            {
+                var decryptionKey = showMasterPasswordDialog.txtPassword.Password;
+
+                //we decrypt the password file
+
+            }
+        }
+    }
+}
